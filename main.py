dc22c2f80c623f3124dd37d821d6c0bf71a6c924
PatrickM2006 <112640332+PatrickM2006@users.noreply.github.com>
2022-09-30 10:27:10 -0400

    TP2 Reponse a la question 1 et 3 Il reste la question deux a faire

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-
 """
-PROJET FAIT PAR PATRICK MORIN
-29 SEPTEMBRE 2022
-PROJET: JEU DE DEVINETTE
+Projet fait par Patrick Morin
+29 septembre 2022
+Projet: Jeu de Devinette
 """
 import random
 
-chiffre_aleatoire = random.randint(1, 100)
-
-print("J’ai choisi un nombre au hasard entre 1 et 100. ")
-print("Quel est le nombre que j'ai choisi?")
-nombre_essai=0
 
 def restart():
+    """
+    Création de la loop pour recommencer le jeu
+    Tu peux décider avec cette partie de code de soit arreter ou recommencer le jeu
+    """
     recommence = input("Voulez-vous continuer?")
-    if recommence == ("oui"):
+    if recommence == "oui":
         return True
-    elif recommence == ("non"):
+    elif recommence == "non":
         return False
     return restart()
 
+
 while restart():
-        while essai != chiffre_aleatoire:
-            essai = int(input("Insérez un nombre entre 1 et 100 : "))
-            print(essai)
-            nombre_essai += 1
+    chiffre_aleatoire = random.randint(1, 100)
+    nombre_essai = 0
+    print("J’ai choisi un nombre au hasard entre 1 et 100. ")
+    print("Quel est le nombre que j'ai choisi?")
+    while (essai := int(input("Insérez un nombre entre 1 et 100 : "))) != chiffre_aleatoire:
+        nombre_essai += 1
+        if essai < chiffre_aleatoire:
+            print("Mauvaise réponse, le nombre est plus grand ")
 
-            if essai < chiffre_aleatoire :
-                print ("Mauvaise réponse, le nombre est plus grand ")
+        elif essai > chiffre_aleatoire:
+            print("Mauvaise réponse, le nombre est plus petit ")
 
-            elif essai > chiffre_aleatoire :
-                print ("Mauvaise réponse, le nombre est plus petit ")
-
-        print(f"Bravo! Bonne réponse! Vous avez réussi en {nombre_essai} essais" )
+    print("Bravo! Bonne réponse! Vous avez réussi en ", nombre_essai, " essais")
